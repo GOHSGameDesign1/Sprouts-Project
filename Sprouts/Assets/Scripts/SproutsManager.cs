430ca1f8d6ebdbc464a3e18ef68b3bce8ff71eda
GOHSGameDesign1 <GOHSGameDesign1@gmail.com>
2023-10-27 23:45:03 -0700

    Added a structured startup function

--- Sprouts/Assets/Scripts/SproutsManager.cs
+++ Sprouts/Assets/Scripts/SproutsManager.cs
@@ -1,19 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class SproutsManager : MonoBehaviour
 {
-    List<Land> lands = new List<Land>();
+    List<Land> _lands = new List<Land>();
     // Start is called before the first frame update
     void Start()
     {
-        
+        CreateEmptyPosition(3);
+        Debug.Log("Lands: " + _lands.Count() + ".  Regions: " + _lands[0].regions.Count() + ".  Boundaries: " + _lands[0].regions[0].boundaries.Count());
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CreateEmptyPosition(int numSpots)
     {
-        
+        List<Region> regions = new List<Region>();
+        List<Boundary> boundaries = new List<Boundary>();
+        for(int i = 0; i < numSpots; i++)
+        {
+            List<Spot> tempSpotList = new List<Spot>() { new Spot() };
+            boundaries.Add(new Boundary(tempSpotList));
+        }
+        regions.Add(new Region(boundaries));
+
+        _lands.Add(new Land(regions));
     }
 }
